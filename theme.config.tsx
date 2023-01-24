498b321dae373d8eff5127a494a3d34672a02e74
Abdelmjid Saber <87904671+abdelmjid-saber@users.noreply.github.com>
2023-01-24 19:00:53 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DALL-E</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: '',
   },
   chat: {
-    link: 'https://discord.com',
+    link: '',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/abdelmjid-saber/DALL-E-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'DALL-E Docs',
   },
 }
 
